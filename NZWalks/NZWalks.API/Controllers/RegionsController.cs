b52490c344a47e6e235f14328b3d0b66e2cb3711
ayyappan <ayyappanprofile@gmail.com>
2022-11-06 15:28:59 +0530

    End of Section4.

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -20,12 +20,58 @@ namespace NZWalks.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegionsAsync()
         {
             var regions = await regionRepository.GetAllAsync();           
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
         }
 
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id); 
+            if(region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+
+            return Ok(regionDTO);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.Region region)
+        {
+            if(region == null)
+            {
+                return BadRequest();
+            }
+
+            var regionDomain = mapper.Map<Models.Domain.Region>(region);
+            var addedRegion = await regionRepository.AddAsync(regionDomain);
+            var regionDTOResult = mapper.Map<Models.DTO.Region>(addedRegion);
+
+            return Ok(regionDTOResult);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            var region = await this.regionRepository.DeleteAsync(id);   
+
+            return Ok(region);
+        }
+
+        [HttpPut("")]
+        public async Task<ActionResult> UpdateRegionAsync(Models.DTO.Region region)
+        {            
+            var regionDTo = mapper.Map<Models.Domain.Region>(region);
+            var updatedRegion = await this.regionRepository.UpdateAsync(regionDTo);
+            if(updatedRegion == null) { return BadRequest(); }
+            var regionDTOResult = mapper.Map<Models.DTO.Region>(updatedRegion);
+
+            return Ok(regionDTOResult);
+        }
     }
 }
--- NZWalks/NZWalks.API/Repositories/RegionRepository.cs
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -12,9 +12,49 @@ namespace NZWalks.API.Repositories
         {
             this.nZWalkDbContext = nZWalkDbContext;
         }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            await this.nZWalkDbContext.Regions.AddAsync(region);
+            await this.nZWalkDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region?> DeleteAsync(Guid id)
+        {
+            var region = await this.nZWalkDbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
+            if (region == null) return null;
+            this.nZWalkDbContext.Regions.Remove(region);
+            await this.nZWalkDbContext.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
            return  await this.nZWalkDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region?> GetAsync(Guid id)
+        {
+            return await this.nZWalkDbContext.Regions.FirstOrDefaultAsync(region => region.Id == id);
+        }
+
+        public async Task<Region?> UpdateAsync(Region region)
+        {
+            var existingRegion = await this.nZWalkDbContext.Regions.FirstOrDefaultAsync(r => r.Id == region.Id);
+            if (existingRegion == null) return null;
+            
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Latitude = region.Latitude;
+            existingRegion.Longtitude = region.Longtitude;
+            existingRegion.Population = region.Population;
+
+            this.nZWalkDbContext.Regions.Update(existingRegion);
+            await this.nZWalkDbContext.SaveChangesAsync();
+
+            return existingRegion;
         }
     }
 }
