d640db58e3929063d1e7a6efd67f6a574f65c40a
ayyappan <ayyappanprofile@gmail.com>
2022-11-06 11:00:08 +0530

    End of section3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+using NZWalks.API.Models.DTO;
+using AutoMapper;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync();           
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+            return Ok(regionsDTO);
+        }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalkDbContext nZWalkDbContext;
+
+        public RegionRepository(NZWalkDbContext nZWalkDbContext)
+        {
+            this.nZWalkDbContext = nZWalkDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+           return  await this.nZWalkDbContext.Regions.ToListAsync();
+        }
+    }
+}
